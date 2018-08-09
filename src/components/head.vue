3252028474eb28d1146889f60ab54b23e27865fb
mali18 <mali18@lenovo.com>
2018-08-09 17:20:14 +0800

    修改导航栏，抽取header

---
+++ src/components/head.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class = "ui-header">
+    <el-row :gutter="1" type="flex" class="row-bg el-row-three">
+      <el-col :span="4">
+        <div class="logo-content">
+          <img class="logo" src="../assets/logo.png">
+          <span class="title">LeData</span>
+        </div>
+      </el-col>
+
+      <el-col :span="17">
+        <div class="nav-content">
+          <!-- 导航菜单 -->
+          <el-menu :default-active="activeIndex" class="el_menu" mode="horizontal" @select="handleSelect"
+                    text-color="#fff" active-text-color="#ffd04b" background-color="#545c64">
+            <el-menu-item index="1" class="menu_title">
+              <router-link :to="{name:'userList'}">用户管理</router-link>
+            </el-menu-item>
+            <el-menu-item index="2" class="menu_title">
+              <router-link :to="{name:'mediaList'}">图片管理</router-link>
+            </el-menu-item>
+            <el-submenu index="3" class="menu_title">
+              <template slot="title" class="menu_title">标注管理</template>
+              <el-menu-item index="3-1" class="menu_label">选项1</el-menu-item>
+              <el-menu-item index="3-2" class="menu_label">选项2</el-menu-item>
+              <el-menu-item index="3-3" class="menu_label">选项3</el-menu-item>
+            </el-submenu>
+            <el-submenu index="4" class="menu_title">
+              <template slot="title">标注任务</template>
+              <router-link :to="{name:'taggingImage'}">
+                <el-menu-item index="4-1">标注图片</el-menu-item>
+              </router-link>
+              <el-menu-item index="4-2">选项2</el-menu-item>
+              <el-menu-item index="4-3">选项3</el-menu-item>
+            </el-submenu>
+          </el-menu>
+          <!-- 导航菜单 -->
+        </div>
+      </el-col>
+
+      <el-col :span="3">
+        <div class="user-content">
+          <el-dropdown >
+            <span class="userinfo">User
+              <i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <!-- <el-dropdown-item command="usercenter">个人中心</el-dropdown-item> -->
+              <el-dropdown-item command="logout">注销登录</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div>
+      </el-col>
+      
+    </el-row>
+  </div>
+</template>
+
+<script>
+ export default {
+   data () {
+     return {
+      activeIndex: '2',
+    };
+   },
+   methods: {
+     handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      },
+   },
+   components: {
+
+   }
+ }
+</script>
+
+<style scoped>
+
+ .ui-header{
+   height: 64px;
+   line-height: 64px;
+   width: 100%;
+   background-color: rgb(84, 92, 100);
+   top: 0;
+   left: 0;
+ }
+
+  .logo-content,
+  .nav-content,
+  .user-content {
+    background-color: rgb(84, 92, 100);
+    border-radius: 4px;
+    min-height: 64px;
+    max-height: 64px;
+    min-width: 100px;
+  }
+
+  .user-content{
+    text-align: center;
+  }
+
+  .logo-content > .logo{
+    width: 40px;
+    height: 40px;
+    border-radius: 50px;
+    margin-top: 12px;
+    margin-left: 20px;
+    margin-right: 10px;
+    float: left;
+    position: relative;
+  }
+
+  .logo-content > .title{
+    font-size: 18px;
+    font-weight: bold;
+    color: azure;
+    line-height: 64px;
+  }
+
+  .el-dropdown{
+    width: 100px;
+    height: 64px;
+    text-align: center;
+  }
+
+  .el-dropdown >.userinfo{
+    color: #fff;
+    font-size: 18px;
+  }
+
+  .el-menu {
+    height: 64px;
+    border-right: solid 0px;
+    list-style: none;
+    position: relative;
+    float: right;
+  }
+
+  .menu_title {
+    width: 150px;
+    text-align: center;
+  }
+
+  /* 去掉router-link链接文字的下划线 */
+  a {
+  text-decoration: none;
+  }
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,47 +1,7 @@
 <template>
- <div class="homecontent">
-    <el-container>
-      <el-header>
-        <span class="header_title">Le_data Manager</span>
-        <!-- start ============================== -->
-        <el-menu :default-active="activeIndex"
-          class="el-menu-demo"
-          mode="horizontal"
-          @select="handleSelect"
-          background-color="#545c64"
-          text-color="#fff"
-          active-text-color="#ffd04b">
-
-          <el-menu-item index="1"><router-link :to="{name:'userList'}">用户管理</router-link></el-menu-item>
-          <el-menu-item index="2"><router-link :to="{name:'mediaList'}">图片管理</router-link></el-menu-item>
-          <!-- <el-submenu index="2">
-            <template slot="title">图片管理</template>
-            <router-link :to="{name:'mediaList'}"><el-menu-item index="2-1">图片列表</el-menu-item></router-link>
-
-            <el-menu-item index="2-2">删除图片</el-menu-item>
-          </el-submenu> -->
-          <el-submenu index="3">
-            <template slot="title">标注管理</template>
-            <el-menu-item index="3-1">选项1</el-menu-item>
-            <el-menu-item index="3-2">选项2</el-menu-item>
-            <el-menu-item index="3-3">选项3</el-menu-item>
-          </el-submenu>
-          <el-submenu index="4">
-            <template slot="title">标注任务</template>
-            <router-link :to="{name:'taggingImage'}">
-              <el-menu-item index="4-1">标注图片</el-menu-item>
-            </router-link>
-            <el-menu-item index="4-2">选项2</el-menu-item>
-            <el-menu-item index="4-3">选项3</el-menu-item>
-          </el-submenu>
-        </el-menu>
-        <!-- stop ============================-->
-      </el-header>
-   
-      <el-container>
-        <el-main><router-view></router-view></el-main>
-      </el-container>
-    </el-container>
+  <div class="homecontent">
+    <header-vue></header-vue>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -49,54 +9,18 @@
 export default {
   data() {
     return {
-      activeIndex: '2'
+     
     };
   },
   methods: {
-    handleSelect(key, keyPath) {
-        console.log(key, keyPath);
-      },
+    
   },
-  components: {}
+  components: {
+    
+  }
 };
 </script>
 
 <style scoped>
 
-.el-header,
-.el-footer {
-  background-color: rgb(84, 92, 100);
-  color: #333;
-  width: 100%;
-  line-height: 60px;
-  float: left;
-  top: 0;
-}
-.header_title {
-  font-weight: bold;
-  color: aliceblue;
-}
-.el-main {
-  height: 100%;
-  background-color: #e9eef3;
-  color: #333;
-  text-align: center;
-  line-height: 160px;
-}
-.header_title {
-  float: left;
-}
-
-.el-menu {
-  border-right: solid 1px #e6e6e6;
-  list-style: none;
-  position: relative;
-  margin: 0;
-  padding-left: 0;
-  float: right;
-}
-
-a {
-  text-decoration: none;
-}
 </style>
