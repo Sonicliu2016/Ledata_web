57332052aecfa6b5cdf14a6d93bed13e81c9b7fc
mali18 <mali18@lenovo.com>
2018-09-29 10:09:42 +0800

    增加重标和保存接口联调

--- src/pages/task/evaluateTaskList.vue
+++ src/pages/task/evaluateTaskList.vue
@@ -12,8 +12,8 @@
                 标注者：{{task.UserName}}<br/>
                 <el-button  type="text" class="button" @click="checkSingleImg(task)">单图检查</el-button>
                 <el-button  type="text" class="button" @click="checkSingleLabel(task)">单标签检查</el-button><br/>
-                <el-button type="danger" plain size="small" @click="showAssignTaskDialog(tag)">重标</el-button>
-                <el-button type="success" plain size="small" @click="showAssignTaskDialog(tag)">保存</el-button>
+                <el-button type="danger" plain size="small" @click="relabelEvaluateTaskToAnnotate(task)">重标</el-button>
+                <el-button type="success" plain size="small" @click="submitEvaluateTask(task)">保存</el-button>
               </div>
             </el-card>
           </el-col>
@@ -54,21 +54,63 @@ export default {
     },
     getEvaluteTaskList(){
       var params = new URLSearchParams();
-        this.$axios({
-            method: 'post',
-            url:"/task/getEvaluateTasks",
-            data:params
-        })
-        .then(res => {
-          if(res.data.code == 200){
-            this.evaluateTaskList = res.data.data.tasks;
-          }else{
-          }
-        })
-        .catch(err => {
-          console.log("error:" + err);
-          // alert("服务器出现故障，请稍后再试！");
-        });
+      this.$axios({
+          method: 'post',
+          url:"/task/getEvaluateTasks",
+          data:params
+      })
+      .then(res => {
+        if(res.data.code == 200){
+          this.evaluateTaskList = res.data.data.tasks;
+        }else{
+        }
+      })
+      .catch(err => {
+        console.log("getEvaluteTaskList , error:" + err);
+        // alert("服务器出现故障，请稍后再试！");
+      });
+    },
+    relabelEvaluateTaskToAnnotate(task){
+      var params = new URLSearchParams();
+      params.append("taskid",task.TaskUniqueId);
+      this.$axios({
+          method: 'post',
+          url:"/task/relabelEvaluateTaskToAnnotate",
+          data:params
+      })
+      .then(res => {
+        if(res.data.code == 200){
+          console.log("请求成功，重标");
+          this.getEvaluteTaskList();
+        }else{
+          console.log("请求失败，重标 code"+res.data.code);
+        }
+      })
+      .catch(err => {
+        console.log("relabelEvaluateTaskToAnnotate , error:" + err);
+        // alert("服务器出现故障，请稍后再试！");
+      });
+    },
+    submitEvaluateTask(task){
+      var params = new URLSearchParams();
+      params.append("taskid",task.TaskUniqueId);
+      this.$axios({
+          method: 'post',
+          url:"/task/submitEvaluateTask",
+          data:params
+      })
+      .then(res => {
+        if(res.data.code == 200){
+          console.log("请求成功，保存");
+          this.getEvaluteTaskList();
+        }else{
+          console.log("请求失败，保存 code"+res.data.code);
+        }
+      })
+      .catch(err => {
+        console.log("submitEvaluateTask , error:" + err);
+        // alert("服务器出现故障，请稍后再试！");
+      });
     }
   },
   created(){
