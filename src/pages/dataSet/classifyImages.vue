a7ba724153646a22e382fb31bfc46253d0b750c2
0914yang <1392265723@qq.com>
2019-03-01 15:25:04 +0800

    数据集分页显示全部图片

--- src/pages/dataSet/classifyImages.vue
+++ src/pages/dataSet/classifyImages.vue
@@ -3,15 +3,18 @@
   <h1 style="text-align:center;margin-bottom:10px;">
     {{className}}
   </h1>
-
+  <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange;getMediasByClusterName()" :current-page.sync="currentPage" layout="prev, pager, next, jumper" :total="this.pageToatl">
+  </el-pagination>
   <el-row :gutter="20">
     <el-col :span="4" v-for="(img,index) in images" :key="index">
       <img class="small-img" v-bind:src=baseurl+img.media_url @click="handleClick(img.media_url)">
-      </el-col>
+    </el-col>
   </el-row>
   <Modal title="View Image" v-model="showModal" footer-hide>
     <img class="big-img" :src="clickedImage" v-if="showModal" style="width: 100%">
-    </Modal>
+  </Modal>
+  <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange;getMediasByClusterName()" :current-page.sync="currentPage" layout="prev, pager, next, jumper" :total="this.pageToatl">
+  </el-pagination>
 </div>
 </template>
 
@@ -26,6 +29,9 @@ export default {
       images: [],
       showModal: false,
       timestamp: '',
+      currentPage: 1,
+      pageToatl: '',
+      pageSize: 200,
     }
   },
   methods: {
@@ -34,14 +40,21 @@ export default {
       this.showModal = true;
     },
     getMediasByClusterName() {
+
       var params = new URLSearchParams();
       params.append("clusterName", this.className);
+      params.append("pageIndex", this.currentPage);
+      params.append("pageSize", this.pageSize);
       this.$axios({
           method: 'post',
-          url: '/label/getMediasByClusterName',
+          url: '/label/getPageMediasByClusterName',
           data: params
         })
         .then(res => {
+          this.$message({
+            message: '图片正在加载中，请稍等',
+            showClose: true,
+          });
           // this.images = res.data.data.ok_medias;
           // console.log("this.allTagsList: " + this.images.length);
           var okMedias = res.data.data.ok_medias;
@@ -61,9 +74,11 @@ export default {
   },
   created() {
     this.className = this.$route.params.tagId;
+    this.pageToatl = Math.ceil(this.$route.params.tagCount / 200) * 10;
     this.baseurl = Global.BASE_URL;
     this.getMediasByClusterName();
-    console.log("classifyImages : tagid : " + this.className + " v" + this.baseurl);
+
+    console.log("classifyImages : tagid : " + this.className + " pageToatl :" + this.pageToatl + " v" + this.baseurl);
   }
 }
 </script>
@@ -71,6 +86,7 @@ export default {
 <style scoped>
 .classifyimages {
   padding: 30px;
+  text-align: center;
 }
 
 .small-img {
